cabd80a583437f1f453fd34fe5959691b0c1379e
Victor <victornjoroge2020@gmail.com>
2023-06-19 21:25:36 +0300

    done

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((word) => {
+    let eachWord = word.split(" ");
+    let newArr = [];
+    for (let i = 0; i < eachWord.length; i++) {
+      let upperCased =eachWord[i].charAt(0).toUpperCase() + eachWord[i].slice(1);
+      newArr.push(upperCased);
+    }
+    return newArr.join(" ");
+  })
 }
+console.log(titleCased());
